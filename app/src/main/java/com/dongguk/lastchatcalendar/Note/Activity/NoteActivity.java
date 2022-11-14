0dcc35e34a90cf5bfd5387e6f0c6f4c46ace1647
Minwoo <okkilltogo@gmail.com>
2022-11-14 12:36:15 +0900

    끝

--- app/src/main/java/com/dongguk/lastchatcalendar/Note/Activity/NoteActivity.java
+++ app/src/main/java/com/dongguk/lastchatcalendar/Note/Activity/NoteActivity.java
@@ -1,20 +1,35 @@
 package com.dongguk.lastchatcalendar.Note.Activity;
 
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.util.Patterns;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.dongguk.lastchatcalendar.Note.adapters.NotesAdapter;
 import com.dongguk.lastchatcalendar.Note.database.NotesDatabase;
@@ -32,12 +47,16 @@ public class NoteActivity extends AppCompatActivity implements NotesListener {
     public static final int REQUEST_CODE_ADD_NOTE = 1;
     public static final int REQUEST_CODE_UPDATE_NOTE = 2;
     public static final int REQUEST_CODE_SHOW_NOTES = 3;
+    public static final int REQUEST_CODE_SELECT_IMAGE = 4;
+    public static final int REQUEST_CODE_STORAGE_PERMISSION = 5;
 
     private RecyclerView notesRecyclerView;
     private List<Note> noteList;
     private NotesAdapter notesAdapter;
 
     private int noteClickedPosition = -1;
+
+    private AlertDialog dialogAddURL;
 
 
 
@@ -49,6 +68,7 @@ public class NoteActivity extends AppCompatActivity implements NotesListener {
         binding = ActivityNoteBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         setListeners();
+
 //        ImageView imageAddNoteMain = findViewById(R.id.imageAddNoteMain);
 //        imageAddNoteMain.setOnClickListener((v)-> {
 //            startActivityForResult(
@@ -89,6 +109,79 @@ public class NoteActivity extends AppCompatActivity implements NotesListener {
 
             }
         });
+
+        //Quick Action 버튼
+        findViewById(R.id.imageAddNote).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(
+                    new Intent(getApplicationContext(), CreateNoteActivity.class),
+                    REQUEST_CODE_ADD_NOTE
+            );
+
+            }
+        });
+
+        findViewById(R.id.imageAddImage).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if(ContextCompat.checkSelfPermission(
+                        getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE
+                )!= PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(
+                            NoteActivity.this,
+                            new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},
+                            REQUEST_CODE_STORAGE_PERMISSION
+                    );
+                }else{
+                    selectImage();
+                }
+
+            }
+        });
+
+        findViewById(R.id.imageAddWebLink).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                showAddURLDialog();
+            }
+        });
+    }
+
+    //사진 선택 로직
+    private void selectImage(){
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        if(intent.resolveActivity(getPackageManager()) != null){
+            startActivityForResult(intent, REQUEST_CODE_SELECT_IMAGE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode == REQUEST_CODE_STORAGE_PERMISSION && grantResults.length > 0){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                selectImage();
+            }else{
+                Toast.makeText(this, "이미지 선택 불가능!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    //이미지끌어와서 db에 넣기
+    private String getPathFromUri(Uri contentUri){
+        String filepath;
+        Cursor cursor= getContentResolver()
+                .query(contentUri, null, null, null, null);
+        if(cursor == null){
+            filepath = contentUri.getPath();
+        }else{
+            cursor.moveToFirst();
+            int index = cursor.getColumnIndex("_data");
+            filepath = cursor.getString(index);
+            cursor.close();
+        }
+        return filepath;
     }
 
     @Override
@@ -165,6 +258,68 @@ public class NoteActivity extends AppCompatActivity implements NotesListener {
                 //업데이트가 이미 되었으니 db 에서 사용 가능, isNoteDeleted key 값도 부여됨. 10.3
                 getNotes(REQUEST_CODE_UPDATE_NOTE, data.getBooleanExtra("isNoteDeleted", false));
             }
+            //Quick button 로직
+        }else if(requestCode == REQUEST_CODE_SELECT_IMAGE && resultCode == RESULT_OK){
+            if(data != null){
+                Uri selectedImageUri = data.getData();
+                if(selectedImageUri != null){
+                    try{
+                        String selectedImagePath = getPathFromUri(selectedImageUri);
+                        Intent intent = new Intent(getApplicationContext(), CreateNoteActivity.class);
+                        intent.putExtra("isFromQuickActions", true);
+                        intent.putExtra("quickActionType", "image");
+                        intent.putExtra("imagePath", selectedImagePath);
+                        startActivityForResult(intent, REQUEST_CODE_ADD_NOTE);
+                    }catch (Exception exception){
+                        Toast.makeText(this, exception.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+
         }
+    }
+    //URL 다이아로그 생성 로직
+    private void showAddURLDialog(){
+        if(dialogAddURL== null){
+            AlertDialog.Builder builder = new AlertDialog.Builder(NoteActivity.this);
+            View view = LayoutInflater.from(this).inflate(
+                    R.layout.layout_add_url,
+                    (ViewGroup) findViewById(R.id.layoutAddUrlContainer)
+            );
+            builder.setView(view);
+
+            dialogAddURL = builder.create();
+            if(dialogAddURL.getWindow() != null){
+                dialogAddURL.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+            }
+            final EditText inputURL = view.findViewById(R.id.inputURL);
+            inputURL.requestFocus();
+
+            view.findViewById(R.id.textAdd).setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    if(inputURL.getText().toString().trim().isEmpty()){
+                        Toast.makeText(NoteActivity.this, "URL을 입력하세요.", Toast.LENGTH_SHORT).show();
+                    }else if(!Patterns.WEB_URL.matcher(inputURL.getText().toString()).matches()){
+                        Toast.makeText(NoteActivity.this, "잘못된 URL을 입력했습니다.", Toast.LENGTH_SHORT).show();
+                    }else {
+                        dialogAddURL.dismiss();
+                        Intent intent = new Intent(getApplicationContext(), CreateNoteActivity.class);
+                        intent.putExtra("isFromQuickActions", true);
+                        intent.putExtra("quickActionType", "URL");
+                        intent.putExtra("URL", inputURL.getText().toString());
+                        startActivityForResult(intent, REQUEST_CODE_ADD_NOTE);
+                    }
+                }
+            });
+
+            view.findViewById(R.id.textCancel).setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    dialogAddURL.dismiss();
+                }
+            });
+        }
+        dialogAddURL.show();
     }
 }
